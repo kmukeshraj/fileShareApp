1ae1abdf16ef9e660e7029ebae1cc43f17208b38
kmukeshraj <mukeshr71@gmail.com>
2021-07-22 23:49:24 +0530

    Server and db connection added

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,1 +1,8 @@
-console.log('server');
+const express= require('express');
+const app = express();
+
+const PORT=process.env.PORT || 3000;
+
+app.listen(PORT,()=> {
+console.log('server started at 3000');
+})
