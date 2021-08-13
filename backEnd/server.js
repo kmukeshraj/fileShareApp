93d9c02846e9154a4db8e5ebcd7172dc471304ea
kmukeshraj <mukeshr71@gmail.com>
2021-08-13 22:34:21 +0530

    handling files with multer

--- backEnd/server.js
+++ backEnd/server.js
@@ -1,7 +1,24 @@
 const express= require('express');
+const connectDB = require('./config/db');
 const app = express();
 
 const PORT=process.env.PORT || 3000;
+ require('./config/db');
+ connectDB();
+
+ app.use(function (req, res, next) {
+    //Enabling CORS
+    res.header("Access-Control-Allow-Origin", "*");
+    res.header("Access-Control-Allow-Methods", "GET,HEAD,OPTIONS,POST,PUT");
+    res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept, x-client-key, x-client-token, x-client-secret, Authorization");
+      next();
+    }); 
+
+
+
+ //Routes
+ app.use('/api/files', require('./routes/files'));
+
 
 app.listen(PORT,()=> {
 console.log('server started at 3000');
